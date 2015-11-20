a3908a7d80a3c79c82ce2003d92ea1f1baa518cd
Irina <irina.solomoni2@gmail.com>
2015-11-20 14:43:37 +0200

    Initial commit

---
+++ app/src/main/java/com/isolomonik/toolbaraction/MainListDetailActivity.java
@@ -0,0 +1,35 @@
+package com.isolomonik.toolbaraction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MainListDetailActivity extends AppCompatActivity {
+ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        listView =(ListView) findViewById(R.id.listView);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
